51611d11fda16c3daf9eda09bb500714203c2232
MarvinCalmer <144715980+MarvinCalmer@users.noreply.github.com>
2025-12-20 21:39:50 +0100

    implemented playlists logic

--- src/core/playlist.py
+++ src/core/playlist.py
@@ -1,20 +1,55 @@
 from dataclasses import dataclass
-from typing import List
-from .title import Title
 
 
 @dataclass
 class Playlist:
     """
-    Represents a Playlist which contains multiple Title objects
+    Represents a playlist containing references to Title objects.
+
+    Attributes
+    ----------
+    id : int or None
+        Unique identifier of the playlist.
+    name : str
+        Name of the playlist.
+    description : str
+        Optional description of the playlist.
+    title_ids : List[int]
+        List of title IDs that are in this playlist (references to Title objects).
     """
 
-    id: int | None
-    titles: List[Title]
+    def __init__(self, id=None, name="", description="", title_ids=None):
+        """
+        Initialize a Playlist instance.
 
-    def __init__(self, id=None, titles=None):
+        Parameters
+        ----------
+        id : int or None, optional
+            ID of the playlist. If None, will be assigned automatically.
+        name : str, optional
+            Name of the playlist.
+        description : str, optional
+            Description of the playlist.
+        title_ids : List[int], optional
+            List of title IDs in the playlist.
+        """
         self.id = id
-        self.titles = titles if titles is not None else []
+        self.name = name
+        self.description = description
+        self.title_ids = title_ids if title_ids is not None else []
 
     def to_dict(self):
-        return {"id": self.id, "titles": [title.to_dict() for title in self.titles]}
+        """
+        Convert the Playlist object into a serializable dictionary.
+
+        Returns
+        -------
+        dict
+            A dictionary containing all playlist fields.
+        """
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "title_ids": self.title_ids,
+        }
